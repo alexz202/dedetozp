040023cc77282a971047ee3b658f041249f831f1
alexz <alexz@example.com>
2014-12-07 21:56:15 +0800

    first push pj files

---
+++ mng/data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'dedetozp';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'carman123';
+$cfg_dbprefix = 'tp_';
+$cfg_db_language = 'utf8';
+
+
+?>
