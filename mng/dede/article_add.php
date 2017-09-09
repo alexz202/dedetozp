67b0651d6b22a6844bb0cfda817cf3237c68d101
alexz <alexz@example.com>
2017-09-09 16:26:30 +0800

    wq wqo

--- mng/dede/article_add.php
+++ mng/dede/article_add.php
@@ -92,7 +92,7 @@ else if($dopost=='save')
 
     //对保存的内容进行处理
     if(empty($writer))$writer=$cuserLogin->getUserName();
-    if(empty($source))$source='未知';
+    if(empty($source))$source='周浦人大';
     $pubdate = GetMkTime($pubdate);
     $senddate = time();
     $sortrank = AddDay($pubdate,$sortup);
