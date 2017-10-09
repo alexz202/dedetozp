e703953b3d7ec2db7b525435769b40faf0b51650
zjy <154364351@qq.com>
2017-10-09 18:15:20 +0800

    add qrcode

---
+++ weixin/app/Lib/ORG/Makeqrcode.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zjy202
+ * Date: 2017/10/9
+ * Time: 17:41
+ */
+require "phpqrcode/phpqrcode.php";
+class Makeqrcode {
+    public static function png($text,$filename=false, $errorCorrectionLevel=0, $matrixPointSize=3,$margin=4){
+       return  QRcode::png($text,$filename, $errorCorrectionLevel, $matrixPointSize, $margin);
+    }
+}
