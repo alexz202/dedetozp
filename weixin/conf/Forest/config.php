9b7211cdaf5ca956f2f5f271e4a391a8b0106d7d
alexz <alexz@example.com>
2017-12-02 21:28:03 +0800

    modify

--- weixin/conf/Forest/config.php
+++ weixin/conf/Forest/config.php
@@ -9,8 +9,11 @@ define('WETLANDINFO_I',50);//50
 define('ANIMALPROTECT_I',51);//51
 define('PREVENTION_I',52);//52
 define('ZHIWUKEPU_I',53);//53
-define('ENROLL_I',54);//54
+define('SHEHUILVDI_I',57);//57
+
 define('ACTIVEREVIEW_I',55);//55
+define('ENROLL_I',54);//54
+
 
 
 define('HOTREPO','hotrepo'); //43
@@ -22,8 +25,11 @@ define('WETLANDINFO','wetlandinfo');//50
 define('ANIMALPROTECT','animalprotect');//51
 define('PREVENTION','prevention');//52
 define('ZHIWUKEPU','zhiwukepu');//53
+define('SHEHUILVDI','shehuilvdi');//53
 define('ENROLL','enroll');//54
 define('ACTIVEREVIEW','activereview');//55
+
+
 
 /**
  * Created by PhpStorm.
@@ -46,11 +52,12 @@ return array(
     'SERVICE'=>array(
 		array('value'=>'公园绿地','id'=>PARKSEINFO_I,'nav'=>1),
         array('value'=>'古树资源','id'=>TREEINFO_I,'nav'=>1),
-        array('value'=>'林业资源','id'=>FORESTINFO_I,'nav'=>1),
+        array('value'=>'森林资源','id'=>FORESTINFO_I,'nav'=>1),
         array('value'=>'湿地资源','id'=>WETLANDINFO_I,'nav'=>1),
 		array('value'=>'野生动物保护','id'=>ANIMALPROTECT_I,'nav'=>1),
 		array('value'=>'病虫害预警','id'=>PREVENTION_I,'nav'=>1),
 		array('value'=>'常见植物科普','id'=>ZHIWUKEPU_I,'nav'=>1),
+		array('value'=>'社会绿地','id'=>SHEHUILVDI_I,'nav'=>1),
     ),
 	'HUDONG'=>array(
 		array('value'=>'报名互动','id'=>ENROLL_I,'nav'=>1),
