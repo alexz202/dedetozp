0eba7aca69688e94828a822860f2fbc446d856cd
alexz <alexz@example.com>
2018-04-18 00:24:26 +0800

    11

--- weixin/conf/Forest/config.php
+++ weixin/conf/Forest/config.php
@@ -10,6 +10,7 @@ define('ANIMALPROTECT_I',51);//51
 define('PREVENTION_I',52);//52
 define('ZHIWUKEPU_I',53);//53
 define('SHEHUILVDI_I',57);//57
+define('ZECEJIEDU_I',58);//57
 
 define('ACTIVEREVIEW_I',55);//55
 define('ENROLL_I',54);//54
@@ -28,7 +29,6 @@ define('ZHIWUKEPU','zhiwukepu');//53
 define('SHEHUILVDI','shehuilvdi');//53
 define('ENROLL','enroll');//54
 define('ACTIVEREVIEW','activereview');//55
-
 
 
 /**
@@ -58,6 +58,7 @@ return array(
 		array('value'=>'病虫害预警','id'=>PREVENTION_I,'nav'=>1),
 		array('value'=>'常见植物科普','id'=>ZHIWUKEPU_I,'nav'=>1),
 		array('value'=>'社会绿地','id'=>SHEHUILVDI_I,'nav'=>1),
+		array('value'=>'政策解读','id'=>ZECEJIEDU_I,'nav'=>1),
     ),
 	'HUDONG'=>array(
 		array('value'=>'报名互动','id'=>ENROLL_I,'nav'=>1),
