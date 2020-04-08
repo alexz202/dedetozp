4d561b80ad76d6d584c84622f5f7143563f2a294
zjy202 <alexz202@hotmail.com>
2020-04-08 14:36:00 +0800

    11

--- weixin/app/Lib/Action/Zp/IndexAction.class.php
+++ weixin/app/Lib/Action/Zp/IndexAction.class.php
@@ -149,16 +149,20 @@ class IndexAction extends BaseAction
         $zpstyle = C('ZPSTYLE');
         $this->assign('active', $zpstyle[1]['value']);
         $this->assign('keywords', $zpstyle[1]['key']);
-        $member_belong = M('member_belong');
-        $list = $member_belong->order('type asc')->select();
-        $list_ = array();
-        foreach ($list as $k => $v) {
-            $list_[$v['type']][] = array('id' => $v['id'],
-                'name' => $v['name'],
-                'type' => $v['type']
-            );
-        }
-        $this->assign('list', $list_);
+        $bid=50;
+        $zpinfo = M('zpinfo');
+        $condition['belong'] = $bid;
+        $condition['arcrank'] = 0;
+        $count_ = $zpinfo->where($condition)->count();
+        import('ORG.Util.Page');
+        $page = new Page($count_, C('PAGERSIZE'));
+        $page->setConfig('theme', "%upPage%   %downPage% ");
+        $list = $zpinfo->where($condition)->order('id desc')->limit($page->firstRow . ',' . $page->listRows)->select();
+        // var_dump($list);
+        $show = $page->show();
+        $this->assign('commentlist', $list);
+        $this->assign('page', $show);
+        $this->assign('count', $count_);
         $this->display();
 //        var_dump($list_);
     }
